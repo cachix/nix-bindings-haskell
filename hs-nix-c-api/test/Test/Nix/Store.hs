31c1c4d26e4a71759631f5b4ba3e98924c688b6c
Sander <hey@sandydoo.me>
2026-03-26 19:44:31 +0100

    fix: use "auto" store in tests

--- hs-nix-c-api/test/Test/Nix/Store.hs
+++ hs-nix-c-api/test/Test/Nix/Store.hs
@@ -13,39 +13,39 @@ spec :: Spec
 spec = describe "Nix.Store" $ before_ initNix $ do
   describe "withStore" $ do
     it "can open and close the local store" $ do
-      withStore "daemon" $ \_ -> pure ()
+      withStore "auto" $ \_ -> pure ()
 
     it "can open the daemon store" $ do
-      withStore "daemon" $ \_ -> pure ()
+      withStore "auto" $ \_ -> pure ()
 
   describe "storeUri" $ do
     it "returns a non-empty URI" $ do
-      withStore "daemon" $ \store -> do
+      withStore "auto" $ \store -> do
         uri <- storeUri store
         uri `shouldSatisfy` (not . BS.null)
 
   describe "storeDir" $ do
     it "returns the store directory" $ do
-      withStore "daemon" $ \store -> do
+      withStore "auto" $ \store -> do
         dir <- storeDir store
         dir `shouldSatisfy` (not . BS.null)
         dir `shouldSatisfy` ("nix" `BS.isInfixOf`)
 
   describe "storeVersion" $ do
     it "returns a version string" $ do
-      withStore "daemon" $ \store -> do
+      withStore "auto" $ \store -> do
         ver <- storeVersion store
         ver `shouldSatisfy` (not . BS.null)
 
   describe "parseStorePath" $ do
     it "rejects invalid store paths" $ do
-      withStore "daemon" $ \store ->
+      withStore "auto" $ \store ->
         parseStorePath store "not-a-store-path" (\_ -> pure ())
           `shouldThrow` \(_ :: NixError) -> True
 
   describe "isValidPath" $ do
     it "returns False for a non-existent store path" $ do
-      withStore "daemon" $ \store -> do
+      withStore "auto" $ \store -> do
         dir <- storeDir store
         let fakePath = dir <> "/00000000000000000000000000000000-nonexistent"
         parseStorePath store fakePath $ \sp ->
@@ -53,7 +53,7 @@ spec = describe "Nix.Store" $ before_ initNix $ do
 
   describe "storePathName" $ do
     it "extracts the name component of a store path" $ do
-      withStore "daemon" $ \store -> do
+      withStore "auto" $ \store -> do
         dir <- storeDir store
         let fakePath = dir <> "/00000000000000000000000000000000-test-name"
         parseStorePath store fakePath $ \sp -> do
@@ -62,14 +62,14 @@ spec = describe "Nix.Store" $ before_ initNix $ do
 
   describe "openStore/closeStore" $ do
     it "can manually open and close a store" $ do
-      store <- openStore "daemon"
+      store <- openStore "auto"
       uri <- storeUri store
       uri `shouldSatisfy` (not . BS.null)
       closeStore store
 
   describe "parseStorePath'/freeStorePath" $ do
     it "parses and frees a store path" $ do
-      withStore "daemon" $ \store -> do
+      withStore "auto" $ \store -> do
         dir <- storeDir store
         let fakePath = dir <> "/00000000000000000000000000000000-test-free"
         sp <- parseStorePath' store fakePath
@@ -78,6 +78,6 @@ spec = describe "Nix.Store" $ before_ initNix $ do
         freeStorePath sp
 
     it "rejects invalid store paths" $ do
-      withStore "daemon" $ \store ->
+      withStore "auto" $ \store ->
         parseStorePath' store "not-a-store-path"
           `shouldThrow` \(_ :: NixError) -> True
